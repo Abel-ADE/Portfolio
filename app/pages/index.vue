1a6d1abdf5362040b36eb0b6d0acc4d9466e8b77
Abel Iglesias Moure <abeliglesiasmoure@gmail.com>
2026-08-16 18:00:35 +0200

    add new projects

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -35,41 +35,56 @@
 
     <UPageSection id="projects" title="Proyectos" :ui="{ title: 'text-3xl sm:text-3xl lg:text-4xl' }">
       <UPageColumns>
-        <template>
-          <UEmpty title="Escalas de enfermería"
-            description="Web con escalas de enfermería en formato formulario, ofrece la puntuación final de la escala y una pequeña valoración"
-            :actions="[
-              {
-                label: 'Ver proyecto',
-                to: 'https://escalasenfermeria.com/',
-                target: '_blank'
-              },
-              {
-                icon: 'i-lucide:github',
-                label: 'Ver repositorio',
-                color: 'neutral',
-                variant: 'outline',
-                to: 'https://github.com/Abel-ADE/NuxtUi4/',
-                target: '_blank'
-              }
-            ]">
-            <template #description>
-              <p>Web con escalas de enfermería en formato formulario, ofrece la puntuación final de la escala y una
+        <UCard :ui="{ root: 'text-center' }">
+          <template #header>
+            <UBadge label="Desarrollo Web" variant="soft"/>
+          </template>
+          <template #default>
+            <div class="flex flex-col gap-4">
+              <h2 class="text-2xl font-bold">Escalas de enfermería</h2>
+              <p class="text-sm">Web con escalas de enfermería en formato formulario, ofrece la puntuación final de la escala y una
                 pequeña valoración</p>
-              <div class="flex justify-center gap-2 pt-4 pb-2">
-                <MyPopoverIcon size="size-4" nameIcon="vscode-icons:file-type-vscode"
-                  textContent="Visual Studio Code" />
-                <MyPopoverIcon size="size-4" nameIcon="vscode-icons:file-type-html" textContent="HTML 5" />
-                <MyPopoverIcon size="size-4" nameIcon="vscode-icons:file-type-css2" textContent="CSS 3" />
-                <MyPopoverIcon size="size-4" nameIcon="vscode-icons:file-type-js-official" textContent="JavaScript" />
-                <MyPopoverIcon size="size-4" nameIcon="vscode-icons:file-type-vue" textContent="Vue" />
-                <MyPopoverIcon size="size-4" nameIcon="vscode-icons:file-type-nuxt" textContent="Nuxt" />
-                <MyPopoverIcon size="size-4" nameIcon="logos:supabase-icon" textContent="Supabase" />
+              <div class="flex justify-center gap-3">
+                <MyPopoverIcon size="size-5" nameIcon="vscode-icons:file-type-vscode" textContent="Visual Studio Code" />
+                <MyPopoverIcon size="size-5" nameIcon="vscode-icons:file-type-html" textContent="HTML 5" />
+                <MyPopoverIcon size="size-5" nameIcon="vscode-icons:file-type-css2" textContent="CSS 3" />
+                <MyPopoverIcon size="size-5" nameIcon="vscode-icons:file-type-js-official" textContent="JavaScript" />
+                <MyPopoverIcon size="size-5" nameIcon="vscode-icons:file-type-vue" textContent="Vue" />
+                <MyPopoverIcon size="size-5" nameIcon="vscode-icons:file-type-nuxt" textContent="Nuxt" />
+                <MyPopoverIcon size="size-5" nameIcon="logos:supabase-icon" textContent="Supabase" />
               </div>
-            </template>
-          </UEmpty>
-        </template>
-
+            </div>
+          </template>
+          <template #footer>
+            <div class="flex justify-center gap-4">
+              <UButton icon="i-lucide:app-window" size="md" to="https://nuxt-ui4-n6o2p0zgy-abel-ades-projects.vercel.app/" target="_blank" color="primary" variant="outline">Ver proyecto</UButton>
+              <UButton icon="i-lucide-github" size="md" to="https://github.com/Abel-ADE/NuxtUi4/" target="_blank" color="neutral" variant="outline">Ver repositorio</UButton>
+            </div>
+          </template>
+        </UCard>
+        <UCard :ui="{ root: 'text-center' }">
+          <template #header>
+            <UBadge label="Diseño y maquetación Web" variant="soft"/>
+          </template>
+          <template #default>
+            <div class="flex flex-col gap-4">
+              <h2 class="text-2xl font-bold">Barber Shop</h2>
+              <p class="text-sm">Diseño y maquetación web de una página moderna para una barbería local. Ejercicio de Diseño de Aplicaciones Web.</p>
+              <div class="flex justify-center gap-3">
+                <MyPopoverIcon size="size-5" nameIcon="logos:figma" textContent="Figma" />
+                <MyPopoverIcon size="size-5" nameIcon="vscode-icons:file-type-vscode" textContent="Visual Studio Code" />
+                <MyPopoverIcon size="size-5" nameIcon="vscode-icons:file-type-html" textContent="HTML 5" />
+                <MyPopoverIcon size="size-5" nameIcon="vscode-icons:file-type-css2" textContent="CSS 3" />
+              </div>
+            </div>
+          </template>
+          <template #footer>
+            <div class="flex justify-center gap-4">
+              <UButton icon="i-lucide:app-window" size="md" to="https://barber-shop-galician-rustic-j2htzq9kh-abel-ades-projects.vercel.app/" target="_blank" color="primary" variant="outline">Ver proyecto</UButton>
+              <UButton icon="i-lucide-github" size="md" to="https://github.com/Abel-ADE/Barber-Shop-Galician-Rustic" target="_blank" color="neutral" variant="outline">Ver repositorio</UButton>
+            </div>
+          </template>
+        </UCard>
       </UPageColumns>
     </UPageSection>
 
@@ -107,25 +122,27 @@
           }
         },
       ]" />
-      <UButton :ui="{base: 'w-fit mx-auto'}" to="/pdf/abel_iglesias_moure.pdf" target="_blank" icon="tabler:file-cv" size="md" color="neutral" variant="outline">Ver Currículum</UButton>
+      <UButton :ui="{ base: 'w-fit mx-auto' }" to="/pdf/abel_iglesias_moure.pdf" target="_blank" icon="tabler:file-cv"
+        size="md" color="neutral" variant="outline">Ver Currículum</UButton>
     </UPageSection>
 
     <UPageSection id="contact" title="Contacto" description="¿Hablamos?"
       :ui="{ title: 'text-3xl sm:text-3xl lg:text-4xl' }">
       <div class="w-full md:w-1/2 mx-auto flex flex-col gap-4">
-        <UInput readonly v-model="value"
-          :ui="{ trailing: 'pr-0.5', root: 'w-full', base: 'text-center' }">
+        <UInput readonly v-model="value" :ui="{ trailing: 'pr-0.5', root: 'w-full', base: 'text-center' }">
           <template v-if="value?.length" #trailing>
-              <UButton :color="copied ? 'success' : 'neutral'" variant="link" size="sm"
-                :icon="copied ? 'i-lucide-copy-check' : 'i-lucide-copy'" aria-label="Copy to clipboard"
-                @click="copy(value); toast.add({title: '¡Correo electrónico copiado!', description: 'Ahora puedes ir a tu gestor de correo y enviarme un e-mail. Gracias por visitar mi portfolio.', icon:'i-lucide-mail', duration:5000})" />
+            <UButton :color="copied ? 'success' : 'neutral'" variant="link" size="sm"
+              :icon="copied ? 'i-lucide-copy-check' : 'i-lucide-copy'" aria-label="Copy to clipboard"
+              @click="copy(value); toast.add({ title: '¡Correo electrónico copiado!', description: 'Ahora puedes ir a tu gestor de correo y enviarme un e-mail. Gracias por visitar mi portfolio.', icon: 'i-lucide-mail', duration: 5000 })" />
           </template>
         </UInput>
         <div class="w-full flex gap-4 justify-between">
-          <UButton :ui="{base: 'w-full items-center justify-center align-middle'}"  to="https://www.linkedin.com/in/abel-iglesias-moure/" target="_blank" icon="streamline:linkedin"
-            size="md" label="Linkedin" color="neutral" variant="outline"></UButton>
-            <UButton :ui="{base: 'w-full items-center justify-center align-middle'}"  to="https://www.linkedin.com/in/abel-iglesias-moure/" target="_blank" icon="i-lucide-github"
-            size="md" label="Github" color="neutral" variant="outline"></UButton>
+          <UButton :ui="{ base: 'w-full items-center justify-center align-middle' }"
+            to="https://www.linkedin.com/in/abel-iglesias-moure/" target="_blank" icon="streamline:linkedin" size="md"
+            label="Linkedin" color="neutral" variant="outline"></UButton>
+          <UButton :ui="{ base: 'w-full items-center justify-center align-middle' }"
+            to="https://www.linkedin.com/in/abel-iglesias-moure/" target="_blank" icon="i-lucide-github" size="md"
+            label="Github" color="neutral" variant="outline"></UButton>
         </div>
       </div>
     </UPageSection>
